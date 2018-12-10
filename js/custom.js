efa54bb92d6c215f093b35bf20893d68a5d1917c
Ignacio Martin Elias <ignaciomartinelias@gmail.com>
2018-12-10 16:00:45 -0300

    Agregado Texto OS + Algunas Animaciones

--- js/custom.js
+++ js/custom.js
@@ -102,7 +102,30 @@ $(document).ready(function(){
       slidesToScroll: 1,
       autoplay: true,
       autoplaySpeed: 3000,
-      arrows: false
+      arrows: false,
+      responsive: [
+    {
+      breakpoint: 1224,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }
+  ]
     });
 });
 
@@ -121,4 +144,18 @@ $(document).ready(function() {
     $('html, body').animate({ scrollTop: destino.offset().top }, 1500);
     return false;
   });
+});
+
+AOS.init();
+
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 3000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
 });
