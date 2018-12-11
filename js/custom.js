60360ed699c7e51d8ef1769d2f23f0517ab94081
Ignacio Martin Elias <ignaciomartinelias@gmail.com>
2018-12-11 17:14:49 -0300

    Agregado Slider Marcas

--- js/custom.js
+++ js/custom.js
@@ -94,9 +94,11 @@
 
 })(jQuery);
 
-    //------- Slider OS --------// 
+
+    //------- Slider OS y Marcas --------// 
 
 $(document).ready(function(){
+
     $('.slider-os').slick({
       slidesToShow: 6,
       slidesToScroll: 1,
@@ -104,29 +106,61 @@ $(document).ready(function(){
       autoplaySpeed: 3000,
       arrows: false,
       responsive: [
-    {
-      breakpoint: 1224,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    }
-  ]
+        {
+          breakpoint: 1224,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        }
+      ]
     });
+
+    $('.slider-marcas').slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      arrows: false,
+      responsive: [
+        {
+          breakpoint: 1224,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        }
+      ]
+    });
+
 });
 
 
@@ -146,9 +180,6 @@ $(document).ready(function() {
   });
 });
 
-// Inicializar AOS
-AOS.init();
-
 // Contadores
 $('.count').each(function () {
     $(this).prop('Counter',0).animate({
@@ -159,5 +190,14 @@ $('.count').each(function () {
         step: function (now) {
             $(this).text(Math.ceil(now));
         }
+    });
+});
+
+$(document).ready(function() {
+    // Inicializar AOS
+    AOS.init();
+
+    window.addEventListener('load', function() {
+      AOS.refresh();
     });
 });
